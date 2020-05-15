e5afb71d5307b787552a9146bad07e8f7f69cf09
kyle <klovegrove1@gmail.com>
2020-05-14 22:11:08 -0700

    Add bot.py
    Adds python file with limited bot functionality

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+token ='NzA4NDUzMjI0OTg5OTgyNzMx.Xr35Dw.aE3EAjEVuBOW3wHUKt3BQrh2j-4'
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is ready...')
+
+@client.event
+async def on_member_join(member : discord.Member):
+    channel = client.get_channel(708461726080827412)
+    await channel.send(f'{member.mention} has joined the server!')
+
+@client.event
+async def on_member_remove(member : discord.Member):
+    channel = client.get_channel(708461726080827412)
+    await channel.send(f'{member.mention} has left the server...')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! latency: {round(client.latency * 1000)}ms')
+    await ctx.send(f'Channel ID is {ctx.channel.id}')
+
+client.run(token)
