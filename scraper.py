e326d565d60423b5e9a4065c4d1471d940f667d7
kyle <klovegrove1@gmail.com>
2020-05-14 17:13:16 -0700

    Changing to Python
    Deleted all the js files, and
    changed the gitignore file to reflect
    new language

---
+++ scraper.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+class WorkshopMap:
+    def __init__(self):
+        self.name = ''
+        self.author = ''
+        self.description = ''
+        self.imgLink = ''
+        self.mapLink = ''        
+
+def getDescription(script):
+    pattern = '(?<=\"description\"\:).*(?=\,\"user_subscribed\")'
+    found = ''
+
+    script = str(script)
+    # print(script)
+
+    try:
+        found = re.search(pattern, script).group(0)    
+    except AttributeError as err:
+        found = ''
+
+    return found
+
+def getWorkshopMaps():
+    driver = webdriver.Chrome()
+    driver.get('https://steamcommunity.com/workshop/browse/?appid=252950&browsesort=trend&section=readytouseitems&actualsort=trend&p=1&days=-1')
+
+    workshopMaps = set()
+    
+    content = driver.page_source
+    soup = BeautifulSoup(content)
+    for a in soup.findAll('div', attrs={'class':'workshopItem'}):
+        map = WorkshopMap()
+        map.name = a.find('div', attrs={'class':'workshopItemTitle'}).text
+        map.author = a.find('div', attrs={'class':'workshopItemAuthorName'}).find('a').text 
+        map.imgLink = a.find('a', attrs = {'class': 'ugc'}).find('div', attrs = {'class': 'workshopItemPreviewHolder'}).find('img', attrs = {'class': 'workshopItemPreviewImage'})['src']
+        map.mapLink = a.find('a', attrs = {'class': 'ugc'})['href']
+        
+        script = a.next_sibling.next_sibling
+
+        map.description = getDescription(script)
+
+        workshopMaps.add(map)
+        
+        # print(f'Map Name: {map.name}')
+        # print(f'Map Author: {map.author}')
+        # print(f'Tumbnail Link: {map.imgLink}')
+        # print(f'Map Link: {map.mapLink}')
+        # print('---------------------------------\n')
+
+    driver.quit()
+
+    return workshopMaps
+
+x = getWorkshopMaps()
+
+for map in x:
+    print(map.name)
--- workshopScraper.js
+++
@@ -1,7 +0,0 @@
-const siteURL = 'https://steamcommunity.com/workshop/browse/?appid=252950&browsesort=trend&section=readytouseitems&actualsort=trend&p=1&days=-1';
-const axios = require('axios');
-
-const fetchData = async () => {
-    const result = await axios.get(siteURL);
-    return cheerio.load(result.data);
-}
