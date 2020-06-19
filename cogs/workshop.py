fa7e8ec99bf6e0f19f496aa6f750eb5ba97d823e
klovegrove <klovegrove1@gmail.com>
2020-06-19 22:58:53 +0000

    Added remove method
    Adds method to remove nicknames

--- cogs/workshop.py
+++ cogs/workshop.py
@@ -5,6 +5,9 @@ from datetime import date
 from cogs import _scraper, _json
 import asyncio
 
+#TODO: add method for looking up newly added maps
+#         *gonna have to start storing the date the map is added
+
 class Workshop(commands.Cog):
     
     def __init__(self, client):
@@ -18,6 +21,8 @@ class Workshop(commands.Cog):
         # Check how long it's been since an update,
         # if it's been more than a month then we'll update the data
         # and the JSON file
+
+        # I SHOULD MAKE A BACKGROUND TASK FOR THIS
         self.lastUpdated = date.fromisoformat(self._data['lastUpdated'])
         today = date.today()
         timeDelta = today - self.lastUpdated        
@@ -73,12 +78,15 @@ class Workshop(commands.Cog):
         else:
             await ctx.send("I couldn't find that map, sorry!")
 
+    # check for permissions here
+    # or send request to admin?
     @commands.command()
     async def addNickname(self, ctx, *, name):
         map = self.getMapByName(name)
         await ctx.send(f'So you want to add a nickname to the map \"{map.name}\" by {map.author}?')
         await ctx.send('Please confirm by reacting with 👍 or 👎')
 
+        #change this so I don't need to use if statement for emoji check
         def check(reaction, user):
             return user == ctx.author
 
@@ -118,43 +126,141 @@ class Workshop(commands.Cog):
         except asyncio.TimeoutError:
             await ctx.send('👎')
 
+    # check for permissions here
     @commands.command()
     async def removeNickname(self, ctx, *, name):
         map = self.getMapByName(name)
-        await ctx.send(f'Would you like to remove a nickname from  \"{map.name}\" by {map.author}?')
-        await ctx.send('Please confirm by reacting with 👍 or 👎')
+
+        def checkReact(reaction, user):
+            return user == ctx.author and (str(reaction.emoji) == '👍' or str(reaction.emoji) == '👎' )
 
         try:
-            await ctx.send(f'reaction: {reaction}')
-            if str(reaction.emoji) == '👍':
-                nickLength = len(map.nicknames)
-                if nickLength != 1:
-                    def checkUser(msg):
-                        return msg.author == ctx.author
+            if map.nicknames[0] is not name:            
+                await ctx.send(f'Would you like to remove the nickname \"{name}\" from  \"{map.name}\" by {map.author}?')
+                await ctx.send('Please confirm by reacting with 👍 or 👎')
 
-                    if nickLength == 2:
-                        await ctx.send(f'Remove the nickname {map.nicknames[1]}?')
-                        await ctx.send('Please confirm by reacting with 👍 or 👎')
+                reaction, user = await self.client.wait_for('reaction_add', timeout=10, check=checkReact)
+                reactEmoji = str(reaction.emoji)
 
-                        #TODO: check and do shit
+                if str(reaction.emoji) == '👍': 
+                    map.nicknames.remove(name)
+                    await ctx.send("Nickname removed!")
 
-                    else:
-                        message = 'Which nickname would you like to remove? \n'
-                        choiceNum = 1
-                        for name in map.nicknames[1:]:
-                            message += f'{choiceNum} - {name}\n'
-                            choiceNum++
-                        await ctx.send(message)
-                        response = await self.client.wait_for('message', check = checkUser, timeout = 20)
-                        try:
-                            responseInt = int(response.content)
-                            if 
+                    print('Nickname removed, writing to JSON...')
+                    self._data['workshopMaps'] = toDict(self.workshopMaps)
+                    _json.write_json(self._data, 'workshopMaps')
+                    print('...Update Complete!')
                 else:
-                    await ctx.send('It looks like that map has only one nickname so it can\'t be removed.')
+                    await ctx.send('Alright, well thanks for wasting my time I guess...')
             else:
-                await ctx.send('👎')
+                await ctx.send('The default name of the map cannot be removed')
         except asyncio.TimeoutError:
-            await ctx.send('👎')
+            await ctx.send('Looks like you didn\'t respond in time...feelsbadman')
+
+    # check for permissions here
+    @commands.command()
+    async def removeNickname2(self, ctx, *, name):
+        #this is garbage, delete this
+        map = self.getMapByName(name)
+        nickToRemove = None
+        nickLength = len(map.nicknames)
+
+        def checkReact(reaction, user):
+            return user == ctx.author and (str(reaction.emoji) == '👍' or str(reaction.emoji) == '👎' )
+        
+        def checkUser(msg):
+            return msg.author == ctx.author
+            
+        try:
+            if nickLength > 1 and name is not map.nicknames[0]:
+                await ctx.send(f'Would you like to remove the nickname \"{name}\" from  \"{map.name}\" by {map.author}?')
+                await ctx.send('Please confirm by reacting with 👍 or 👎')
+
+                reaction, user = await self.client.wait_for('reaction_add', timeout=10, check=checkReact)
+                reactEmoji = str(reaction.emoji)
+
+                if str(reaction.emoji) == '👍': 
+                    nickToRemove = name 
+                else:
+
+                    await ctx.send(f'Would you like to remove a nickname from  \"{map.name}\" by {map.author}?')
+                    await ctx.send('Please confirm by reacting with 👍 or 👎')
+
+                    reaction, user = await self.client.wait_for('reaction_add', timeout=10, check=checkReact)
+                    reactEmoji = str(reaction.emoji)
+
+                    if str(reaction.emoji) == '👍':                
+                        
+                        if nickLength != 1:
+
+                            if nickLength == 2:
+                                choice = map.nicknames[1]
+                                await ctx.send(f'Remove the nickname {choice}?')
+                                await ctx.send('Please confirm by reacting with 👍 or 👎')
+
+                                reaction, user = await self.client.wait_for('reaction_add', timeout=10, check=checkReact)
+                                reactEmoji = str(reaction.emoji)
+
+                                if reactEmoji == '👍':
+                                    nickToRemove = choice
+                                else:
+                                    await ctx.send('No problem, I promise I won\'t judge you for that')                            
+
+                            else:
+                                message = 'Which nickname would you like to remove? \n'
+                                choiceNum = 1
+                                for name in map.nicknames[1:]:
+                                    message += f'{choiceNum} - {name}\n'
+                                    choiceNum += 1
+                                await ctx.send(message)                        
+                                inputNotValid = True
+                                wrongInputs = 0
+                                while inputNotValid:                            
+                                    try:
+                                        response = await self.client.wait_for('message', check = checkUser, timeout = 20)
+                                        responseInt = int(response.content)
+                                        if responseInt < nickLength and responseInt > 0:
+                                            choice = map.nicknames[responseInt]
+
+                                            await ctx.send(f'Remove the nickname {choice}?')
+                                            await ctx.send('Please confirm by reacting with 👍 or 👎')
+
+                                            reaction, user = await self.client.wait_for('reaction_add', timeout=10.0, check=checkReact)
+                                            reactEmoji = str(reaction.emoji)
+
+                                            if reactEmoji == '👍':
+                                                nickToRemove = choice
+                                                inputNotValid = False
+                                            else:
+                                                inputNotValid = False
+                                                await ctx.send('No problem, I promise I won\'t judge you for that')                                        
+
+                                        else:
+                                            await ctx.send('That choice doesn\'t look quite right')
+                                    except ValueError:
+                                        if wrongInputs < 5:
+                                            await ctx.send('Try putting in a number next time.')
+                                            wrongInputs += 1
+                                        else:
+                                            await ctx.send('Alright my guy, try again when you feel like not being a troll')
+                                            break                                                
+                        else:
+                            await ctx.send('It looks like that map has only one nickname so it can\'t be removed.')
+                    else:
+                        await ctx.send('👎')
+
+                if nickToRemove is not None:
+                    map.nicknames.remove(nickToRemove)
+                    await ctx.send("Nickname removed!")
+
+                    print('Map data updated, writing to JSON...')
+                    self._data['workshopMaps'] = toDict(self.workshopMaps)
+                    _json.write_json(self._data, 'workshopMaps')
+                    print('...Update Complete!')
+            
+        except asyncio.TimeoutError as err:
+            await ctx.send('Looks like you didn\'t respond in time...feelsbadman')
+
 
 # Functions
 
@@ -174,6 +280,10 @@ def getMapEmbed(map : _scraper.WorkshopMap):
     embed.add_field(name = 'Nicknames', value = nicknameStr, inline = False)
 
     return embed
+
+def checkReact(introMsg, exitMsg, user):
+    #need to figure out what I want this to do
+    return None
 
 
 def parseJSON(data):
